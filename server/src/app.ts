0b0b0353540063a8b0fc31b907a7fd4dd0ec604f
Jose Enriquez <120431604+JoseEnriquez88@users.noreply.github.com>
2025-10-25 14:34:02 -0300

    added config app server

---
+++ server/src/app.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import mainRouter from "./routes/mainRouter.routes.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "../swagger.js";
+
+const server = express();
+
+server.use(express.json());
+server.use(morgan("dev"));
+server.use(
+  cors({
+    origin: "http://localhost:5173", //todo: cuando pase a produccion lo cambio a la url del front
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Origin",
+      "X-Requested-With",
+      "Content-Type",
+      "Accept",
+      "Authorization",
+    ],
+  })
+);
+server.use(
+  "/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, {
+    customSiteTitle: "Order Management System",
+  })
+);
+// server.use(express.urlencoded({ extended: true })); //* Para poder recibir datos en el body de las peticiones POST y PUT
+// server.use(express.static("public")); //* Para poder recibir archivos estáticos (imagenes, etc) desde la carpeta public
+server.get("/api/v1/openapi.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpec);
+});
+server.use(mainRouter);
+
+export default server;
