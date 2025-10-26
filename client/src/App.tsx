50a6c46434d202d613f09f7de97e5ec492e46adf
Jose Enriquez <120431604+JoseEnriquez88@users.noreply.github.com>
2025-10-26 16:45:35 -0300

    added tailwind config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,8 @@
-export function App() {
-
+function App() {
   return (
     <>
-      <h1>Holiwis</h1>
+      <h1 className="text-gray-900 font-bold font-ms-bebas text-lg">Holiwis</h1>
     </>
   )
 }
-
+export default App;
